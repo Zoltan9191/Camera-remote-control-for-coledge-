e9a3a900ac6160813505a8bfb48c5e1e591e6c53
Zoltan9191 <Zoltan9191@users.noreply.github.com>
2018-11-27 12:11:01 +0200

    Update kkzRemote.php

--- kkzRemote.php
+++ kkzRemote.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 $ip="192.168.0.101";
 
@@ -10,10 +10,10 @@ $Up="http://admin:admin@".$ip."/cgi-bin/operator/ptzset?move=up";
 $Down="http://admin:admin@".$ip."/cgi-bin/operator/ptzset?move=down";
 $Stop="http://192.168.0.101/cgi-bin/operator/ptzset?move=stop";
 	
-if(isset($_POST["left1"])) { post($Left); post($stop); }
+if(isset($_POST["left1"]))  { post($Left); post($stop); }
 if(isset($_POST["right1"])) { post($Right); post($stop); }
-if(isset($_POST["up1"])) { post($Up); post($stop); }
-if(isset($_POST["down1"])) { post($Down); post($stop); }
+if(isset($_POST["up1"]))    { post($Up); post($stop); }
+if(isset($_POST["down1"]))  { post($Down); post($stop); }
 
 postauth($authent);
 
@@ -30,38 +30,34 @@ function post($Direction = null) {
 ?>
 
 <html>
-  <link rel="stylesheet" type="text/css" href="FB.css">
-
-  <center>
-
   <!-- stream  -->
-
-  <script>
-  updateImage();
-  var a=1;
-
-  function updateImage() {
-      if (a==1) {
-          var x2= document.getElementById("theText2");
-	  x2.src="http://192.168.0.101/jpg/image.jpg?" + Math.random();
-      }
-      setTimeout(updateImage, 370);
-  }
-    </script>
-
+  <head>
+     <link rel="stylesheet" type="text/css" href="FB.css">  
+    <script>
+      var pictureFrame= document.getElementById("pictureTagId");
+      (function updateImage() {
+        if (true) {          
+	    pictureFrame.src="http://192.168.0.101/jpg/image.jpg?" + Math.random();
+        }
+        setTimeout(updateImage, 370);
+       })()
+     </script>
+   </head>
    <!-- video -->
-  <img id="theText2" src="url">
- 
-  <br><br>
+  <body>
+    <center>
+    <img id="pictureTagId" src="">
+    <br><br>
+    <!-- Hidden frame for post w/o page refresh -->
+    <iframe name="HiddenFrame" style="display:none;"></iframe>
 
-  <iframe name="HiddenFrame" style="display:none;"></iframe>
-
-  <!-- buttons -->
-  <form method="post" target="HiddenFrame" >
-      <input id="a1-div-a" type="submit" name="left1" value="Влево">
-      <input  id="a1-div-a" type="submit" name="right1" value="Вправо">
-      <br><br>
-      <input  id="a1-div-a" type="submit" name="up1" value="Вверх">
-      <input  id="a1-div-a" type="submit" name="down1" value="Вниз">
-  </form>
+    <!-- buttons -->
+    <form method="post" target="HiddenFrame" >
+        <input id="a1-div-a" type="submit" name="left1" value="Влево">
+        <input  id="a1-div-a" type="submit" name="right1" value="Вправо">
+        <br><br>
+        <input  id="a1-div-a" type="submit" name="up1" value="Вверх">
+        <input  id="a1-div-a" type="submit" name="down1" value="Вниз">
+    </form>
+  <body>
 </html>
